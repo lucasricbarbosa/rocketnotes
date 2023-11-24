32f11cfd9b6af8f17efe15ce9cdfde24188b27b2
Lucas Ricardo <lucasricardobarb@gmail.com>
2023-11-23 23:42:38 -0300

    first commit

---
+++ rocket-notes/src/pages/Details/index.jsx
@@ -0,0 +1,12 @@
+import { Container } from './styles'
+
+export function Details() {
+
+  return (
+    <Container>
+      <h1>Hello World</h1>
+      <span>Rodrigo Gonçalves</span>
+    </Container>
+  );
+
+}

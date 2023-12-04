7db6fccb98e3b1e5ceb7876c4ddfa94751a157ff
Lucas Ricardo <lucasricardobarb@gmail.com>
2023-12-03 23:32:59 -0300

    adding search component

--- rocket-notes/src/pages/Details/index.jsx
+++ rocket-notes/src/pages/Details/index.jsx
@@ -1,12 +1,47 @@
-import { Container } from './styles'
+import { Container, Links, Content } from "./styles";
+
+import { Header } from "../../components/Header";
+import { Button } from "../../components/Button";
+import { Section } from "../../components/Section";
+import { ButtonText } from "../../components/ButtonText";
+import { Tag } from "../../components/Tag";
 
 export function Details() {
-
   return (
     <Container>
-      <h1>Hello World</h1>
-      <span>Rodrigo Gonçalves</span>
+      <Header />
+
+      <main>
+        <Content>
+          <ButtonText title="Exluir Nota" />
+
+          <h1>Introdução ao React</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur
+            odit non, iste sed aperiam placeat quo iusto consectetur error
+            dolores quia harum, repudiandae rem nostrum tempore assumenda
+            eligendi quisquam eaque.
+          </p>
+
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="#">https://www.rocketseat.com.br</a>
+              </li>
+              <li>
+                <a href="#">https://www.rocketseat.com.br</a>
+              </li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="nodejs" />
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   );
-
 }
